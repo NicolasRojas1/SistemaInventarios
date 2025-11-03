3f4f51c79e00f9797b992a5dca23a2bcdd430d1f
NicolasRojas <nicrojasc6@gmail.com>
2025-11-02 23:41:34 -0500

    ADD separacion de logica y referencias

--- SistemaInventario/Controllers/HomeController.cs
+++ SistemaInventario/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using SistemaInventario.Models;
+using SistemaInventario.Modelos.ViewModels;
 
 namespace SistemaInventario.Controllers
 {
