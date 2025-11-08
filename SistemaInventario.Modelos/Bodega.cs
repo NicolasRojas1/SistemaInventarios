063ae9c538979691e3f67ae1dfdd567b71ed0c3e
NicolasRojas <nicrojasc6@gmail.com>
2025-11-07 19:28:49 -0500

    ADD Modelo Bodega

---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [MaxLength(60, ErrorMessage = "El nombre debe ser de maximo 60 caracteres")]
+        public String Nombre { get; set; }
+
+        [Required(ErrorMessage = "La descripcion es requerido")]
+        [MaxLength(60, ErrorMessage = "La descripcion debe ser de maximo 60 caracteres")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El estado es requerido")]
+        public bool Estado { get; set; }
+    }
+}
